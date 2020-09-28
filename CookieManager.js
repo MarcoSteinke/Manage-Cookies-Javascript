b4a7c15a11e1ed06ecd237e724108c7b473c0ec7
maste150hhu <mastremolten2@gmail.com>
2020-09-28 13:12:40 +0200

    add CookieManager.js

---
+++ CookieManager.js
@@ -0,0 +1,24 @@
+// This code belongs to: https://howchoo.com/javascript/how-to-manage-cookies-in-javascript
+
+// Info: To delete a cookie, just use a call such as setCookie(name, value, -1) ! 
+// By setting the exdays to -1 your cookie will be removed.
+function setCookie(name, value, exdays) {
+    var d, expires;
+    exdays = exdays || 1;
+    d = new Date();
+    d.setTime(d.getTime() + (exdays2460601000));
+    expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + "; " + expires;
+}
+
+function getCookie(name) {
+    var cookie, c;
+    cookies = document.cookie.split(';');
+    for (var i=0; i < cookies.length; i++) {
+        c = cookies[i].split('=');
+        if (c[0] == name) {
+            return c[1];
+        }
+    }
+    return "";
+}
